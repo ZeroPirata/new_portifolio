7c8238d4598af846b2ed88a9b87dd6520c5e8ab7
Gabriel Souza <gabriel0gsbn@gmail.com>
2023-07-28 20:01:25 -0300

    [Add] Imports das páginas
    Importação das páginas dentro do projeto
    
    Inicio da estilização para caber todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
 import NavBar from './components/navbar';
 import './static/global.css'
+import Main from 'pages/Main/Index';
+import { styled } from 'styled-components';
+import About from 'pages/About/Index';
+import Project from 'pages/Project/Index';
+import Contact from 'pages/Contact/Index';
+
+export const AppContainer = styled.body`
+  overflow-x: hidden;
+`;
+
 function App() {
   return (
-    <NavBar />
+    <AppContainer>
+      <NavBar />
+      <Main />
+      <About />
+      <Project />
+      <Contact />
+    </AppContainer>
   );
 }
 
